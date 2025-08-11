45b16610a452a7bbbce7c55d70a531d5ee913cd9
yyhhyyyyyy <yyhhyyyyyy8@gmail.com>
2025-08-11 10:13:17 +0800

    refactor: extract scrollable popover functionality from shadcn ui components

--- src/renderer/src/components/ui/popover/PopoverContent.vue
+++ src/renderer/src/components/ui/popover/PopoverContent.vue
@@ -12,18 +12,16 @@ defineOptions({
 const props = withDefaults(
   defineProps<PopoverContentProps & {
     class?: HTMLAttributes['class']
-    enableScrollable?: boolean
   }>(),
   {
     align: 'center',
-    sideOffset: 4,
-    enableScrollable: false
+    sideOffset: 4
   }
 )
 const emits = defineEmits<PopoverContentEmits>()
 
 const delegatedProps = computed(() => {
-  const { class: _, enableScrollable: __, ...delegated } = props
+  const { class: _, ...delegated } = props
 
   return delegated
 })
@@ -38,15 +36,11 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
       :class="
         cn(
           'z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-          props.enableScrollable && 'max-h-96 overflow-hidden',
           props.class
         )
       "
     >
-      <div v-if="props.enableScrollable" class="max-h-96 overflow-y-auto">
-        <slot />
-      </div>
-      <slot v-else />
+      <slot />
     </PopoverContent>
   </PopoverPortal>
 </template>
