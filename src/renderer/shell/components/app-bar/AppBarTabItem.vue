f8f048ab2c8b2d7eac2efb06eb2e286df10a7e10
zerob13 <zerob13@gmail.com>
2025-10-10 17:31:20 +0800

    chore: settings bg colors

--- src/renderer/shell/components/app-bar/AppBarTabItem.vue
+++ src/renderer/shell/components/app-bar/AppBarTabItem.vue
@@ -3,7 +3,7 @@
     ref="tabItem"
     draggable="true"
     class="shrink-0 text-xs font-medium px-3 h-9 flex items-center justify-between transition-all duration-200 group border-l border-r"
-    :class="[active ? 'bg-bg-card/60 dark:bg-black/60' : '']"
+    :class="[active ? 'bg-bg-card' : '']"
     @dragstart="onDragStart"
     @click="onClick"
   >
