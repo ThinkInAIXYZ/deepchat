a9341b29834a55461881fc9e90c6da3ecf7ef170
zerob13 <zerob13@gmail.com>
2026-01-02 17:38:57 +0800

    refactor(acp): expand barrel exports

--- src/main/presenter/agentPresenter/acp/index.ts
+++ src/main/presenter/agentPresenter/acp/index.ts
@@ -1,8 +1,26 @@
 export type * from './types'
-export { AcpProcessManager } from './acpProcessManager'
-export { AcpSessionManager } from './acpSessionManager'
-export { buildClientCapabilities } from './acpCapabilities'
-export { AcpMessageFormatter } from './acpMessageFormatter'
-export { AgentFileSystemHandler } from './agentFileSystemHandler'
-export { AgentToolManager } from './agentToolManager'
+
+export {
+  AcpProcessManager,
+  type AcpProcessHandle,
+  type SessionNotificationHandler,
+  type PermissionResolver
+} from './acpProcessManager'
+export { AcpSessionManager, type AcpSessionRecord } from './acpSessionManager'
 export { AcpSessionPersistence } from './acpSessionPersistence'
+export { buildClientCapabilities, type AcpCapabilityOptions } from './acpCapabilities'
+export { AcpMessageFormatter } from './acpMessageFormatter'
+export { AcpContentMapper } from './acpContentMapper'
+export { AcpFsHandler } from './acpFsHandler'
+export { AcpTerminalManager } from './acpTerminalManager'
+export { AgentFileSystemHandler } from './agentFileSystemHandler'
+export { AgentToolManager, type AgentToolCallResult } from './agentToolManager'
+export { AgentBashHandler } from './agentBashHandler'
+export {
+  registerCommandProcess,
+  unregisterCommandProcess,
+  terminateCommandProcess
+} from './commandProcessTracker'
+export { getShellEnvironment, clearShellEnvironmentCache } from './shellEnvHelper'
+export { convertMcpConfigToAcpFormat } from './mcpConfigConverter'
+export { filterMcpServersByTransportSupport } from './mcpTransportFilter'
