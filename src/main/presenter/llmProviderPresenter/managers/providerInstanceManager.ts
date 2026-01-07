00d514ac6369a8eecf7e189dccfbe64ad3aa876e
codeyhj <colgateyhj@gmail.com>
2026-01-07 20:08:19 +0800

    feat: add o3.fan provider

--- src/main/presenter/llmProviderPresenter/managers/providerInstanceManager.ts
+++ src/main/presenter/llmProviderPresenter/managers/providerInstanceManager.ts
@@ -34,6 +34,7 @@ import { VercelAIGatewayProvider } from '../providers/vercelAIGatewayProvider'
 import { PoeProvider } from '../providers/poeProvider'
 import { JiekouProvider } from '../providers/jiekouProvider'
 import { ZenmuxProvider } from '../providers/zenmuxProvider'
+import { O3fanProvider } from '../providers/o3fanProvider'
 import { RateLimitManager } from './rateLimitManager'
 import { StreamState } from '../types'
 import { AcpSessionPersistence } from '../../agentPresenter/acp'
@@ -95,7 +96,8 @@ export class ProviderInstanceManager {
       ['aws-bedrock', AwsBedrockProvider],
       ['jiekou', JiekouProvider],
       ['zenmux', ZenmuxProvider],
-      ['acp', AcpProvider]
+      ['acp', AcpProvider],
+      ['o3fan', O3fanProvider]
     ])
   }
 
@@ -127,7 +129,8 @@ export class ProviderInstanceManager {
       ['aws-bedrock', AwsBedrockProvider],
       ['jiekou', JiekouProvider],
       ['zenmux', ZenmuxProvider],
-      ['acp', AcpProvider]
+      ['acp', AcpProvider],
+      ['o3fan', O3fanProvider]
     ])
   }
 
