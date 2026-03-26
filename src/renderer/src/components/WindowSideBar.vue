faf1690ce9e83bd523bab7aa959c4c7cc6e2a2b7
zhangmo8 <wegi866@gmail.com>
2026-03-26 11:32:27 +0800

    chore: update

--- src/renderer/src/components/WindowSideBar.vue
+++ src/renderer/src/components/WindowSideBar.vue
@@ -281,7 +281,7 @@ import { usePresenter, useRemoteControlPresenter } from '@/composables/usePresen
 import { SETTINGS_EVENTS } from '@/events'
 import { useAgentStore } from '@/stores/ui/agent'
 import { useSessionStore, type SessionGroup, type UISession } from '@/stores/ui/session'
-import type { TelegramRemoteStatus } from '@shared/presenter'
+import type { TelegramRemoteStatus, FeishuRemoteStatus, RemoteRuntimeState } from '@shared/presenter'
 import AgentAvatar from './icons/AgentAvatar.vue'
 import WindowSideBarSessionItem from './WindowSideBarSessionItem.vue'
 import { useI18n } from 'vue-i18n'
