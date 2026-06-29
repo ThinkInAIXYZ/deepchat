0f74324380048acf0e9657d1d2e0350dfc782470
yyhhyyyyyy <yyhhyyyyyy8@gmail.com>
2026-06-29 16:15:16 +0800

    chore(deps): migrate to @lucide/vue (#1833)

--- src/shadcn/components/ui/checkbox/Checkbox.vue
+++ src/shadcn/components/ui/checkbox/Checkbox.vue
@@ -2,7 +2,7 @@
 import type { CheckboxRootEmits, CheckboxRootProps } from "reka-ui"
 import type { HTMLAttributes } from "vue"
 import { computed } from "vue"
-import { Check } from "lucide-vue-next"
+import { Check } from "@lucide/vue"
 import { CheckboxIndicator, CheckboxRoot } from "reka-ui"
 import { cn } from '@shadcn/lib/utils'
 
