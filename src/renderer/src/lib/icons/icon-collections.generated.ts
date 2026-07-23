192c82734f8b41fa1fdd8ef9b7cc937967b63336
zhangmo8 <wegi866@gmail.com>
2026-07-23 18:09:42 +0800

    fix(build): stabilize generated icon collections

--- src/renderer/src/lib/icons/icon-collections.generated.ts
+++ src/renderer/src/lib/icons/icon-collections.generated.ts
@@ -758,7 +758,6 @@ export const lucideIconCollection = {
       parent: 'circle-x'
     }
   },
-  lastModified: 1783920389,
   width: 24,
   height: 24
 } as const
@@ -819,14 +818,12 @@ export const vscodeIconCollection = {
     }
   },
   aliases: {},
-  lastModified: 1783920591,
   width: 32,
   height: 32
 } as const
 
 export const lineMdIconCollection = {
   prefix: 'line-md',
-  lastModified: 1767255360,
   aliases: {},
   width: 24,
   height: 24,
